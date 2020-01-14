6ef7f220a5e9a87d2dad05770981c9068a0fc704
Anurag Garg <anurag1106@gmail.com>
2020-01-13 21:20:34 -0600

    fix recusion

--- recursionMaxMin.py
+++ recursionMaxMin.py
@@ -2,7 +2,8 @@ def find_max_min(seq, n):
     if n == 0:
         return seq[0], seq[0]
     else:
-        return max(seq[n], find_max_min(seq, n-1)[0]), min(seq[n], find_max_min(seq, n-1)[1])
+        ma, mi = find_max_min(seq, n - 1)
+        return max(seq[n], ma), min(seq[n], mi)
 
 
 if __name__ == '__main__':

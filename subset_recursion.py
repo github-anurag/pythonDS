785a23dcb4b1a4257b683b500f7ff20a099018b9
Anurag Garg <anurag1106@gmail.com>
2020-01-13 22:06:36 -0600

    powerset using recursion

--- subset_recursion.py
+++ subset_recursion.py
@@ -1,5 +1,16 @@
-def subset(seq):
-    pass
+def power_set(inp):
+    # returns a list of all subsets of the list a
+    if not inp:
+        return [[]]
+    else:
+        main_subset = []
+        for small_subset in power_set(inp[1:]):
+            main_subset += [small_subset]
+            main_subset += [[inp[0]] + small_subset]
+        return main_subset
+
 
 if __name__ == '__main__':
-    pass
+    seq = ['a', 'b', 'c']
+    ans = power_set(seq)
+    print("Subset for {} is {} with len {}".format(seq, ans, len(ans)))

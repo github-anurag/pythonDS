8586786ac524ae986e7159f9d3e7014f50abfc9a
Anurag Garg <anurag1106@gmail.com>
2020-01-13 22:21:46 -0600

    powerset using 2 pow N

--- subset_recursion.py
+++ subset_recursion.py
@@ -10,7 +10,12 @@ def power_set(inp):
         return main_subset
 
 
+def power_set_list(inp):
+    n = len(inp)
+    return [[inp[j] for j in range(n) if i & 1 << j] for i in range(2 ** n)]
+
+
 if __name__ == '__main__':
     seq = ['a', 'b', 'c']
-    ans = power_set(seq)
+    ans = power_set_list(seq)
     print("Subset for {} is {} with len {}".format(seq, ans, len(ans)))
